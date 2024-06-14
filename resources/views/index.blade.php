0c16e82a0bc42a02fbb6c576d8260f6142beadc2
Eric Junior <eric.faria2003@gmail.com>
2024-06-14 02:49:00 -0300

    Criado Dashboard com gráficos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="style.css"> <!-- Link para o CSS customizado -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body class="bg-dark text-white">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -44,34 +45,30 @@
         <h1 class="text-center">Bem-vindo ao Controle de Estufa</h1>
         <p class="text-center">Gerencie sua estufa de forma eficiente e inteligente.</p>
         
-        <!-- Tabela para exibir os dados dos sensores -->
-        <div class="table-responsive mt-4">
-            <table class="table table-dark table-bordered">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Temperatura</th>
-                        <th>Umidade</th>
-                        <th>Soil Moisture</th>
-                        <th>Niveis CO2</th>
-                        <th>Instensidade da Luz</th>
-                        <th>PH</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($sensors as $sensor)
-                        <tr>
-                            <td>{{ $sensor->_id }}</td>
-                            <td>{{ $sensor->temperature }}</td>
-                            <td>{{ $sensor->humidity }}</td>
-                            <td>{{ $sensor->soil_moisture }}</td>
-                            <td>{{ $sensor->co2_levels }}</td>
-                            <td>{{ $sensor->light_intensity }}</td>
-                            <td>{{ $sensor->soil_ph }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <!-- Gráficos -->
+        <div class="row">
+            <div class="col-md-6">
+                <canvas id="temperatureChart"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="humidityChart"></canvas>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <canvas id="soilMoistureChart"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="co2LevelsChart"></canvas>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <canvas id="lightIntensityChart"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="soilPhChart"></canvas>
+            </div>
         </div>
     </div>
 
@@ -88,5 +85,65 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Extrair dados do PHP para JavaScript
+            var sensorData = @json($sensors);
+
+            // Função para criar gráfico
+            function createChart(context, label, data, color) {
+                return new Chart(context, {
+                    type: 'line',
+                    data: {
+                        labels: data.map((_, index) => `Data ${index + 1}`),
+                        datasets: [{
+                            label: label,
+                            data: data,
+                            backgroundColor: color,
+                            borderColor: color,
+                            fill: false,
+                            tension: 0.1
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        scales: {
+                            x: {
+                                display: true,
+                                title: {
+                                    display: true,
+                                    text: 'Time'
+                                }
+                            },
+                            y: {
+                                display: true,
+                                title: {
+                                    display: true,
+                                    text: label
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+
+            // Preparar dados para os gráficos
+            var temperatureData = sensorData.map(sensor => sensor.temperature);
+            var humidityData = sensorData.map(sensor => sensor.humidity);
+            var soilMoistureData = sensorData.map(sensor => sensor.soil_moisture);
+            var co2LevelsData = sensorData.map(sensor => sensor.co2_levels);
+            var lightIntensityData = sensorData.map(sensor => sensor.light_intensity);
+            var soilPhData = sensorData.map(sensor => sensor.soil_ph);
+
+            // Criar gráficos
+            createChart(document.getElementById('temperatureChart').getContext('2d'), 'Temperatura', temperatureData, 'rgba(255, 99, 132, 1)');
+            createChart(document.getElementById('humidityChart').getContext('2d'), 'Umidade', humidityData, 'rgba(54, 162, 235, 1)');
+            createChart(document.getElementById('soilMoistureChart').getContext('2d'), 'Soil Moisture', soilMoistureData, 'rgba(75, 192, 192, 1)');
+            createChart(document.getElementById('co2LevelsChart').getContext('2d'), 'CO2 Levels', co2LevelsData, 'rgba(153, 102, 255, 1)');
+            createChart(document.getElementById('lightIntensityChart').getContext('2d'), 'Intensidade da Luz', lightIntensityData, 'rgba(255, 159, 64, 1)');
+            createChart(document.getElementById('soilPhChart').getContext('2d'), 'pH do Solo', soilPhData, 'rgba(255, 206, 86, 1)');
+        });
+    </script>
 </body>
 </html>
